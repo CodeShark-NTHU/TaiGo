fc44c29cda928b5bf4fe1f21109a3a1ee67ff37d
bhimasta <agoengbhimasta@gmail.com>
2017-10-15 16:59:07 +0800

    adding class most_api, city_stop, and bus_stop. Also adding API test for it

---
+++ lib/bus_stop.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: false
+
+module PublicTrans
+  # Provides access to bus stop data
+  class BusStop
+    def initialize(data)
+      @stop = data
+    end
+
+    def uid
+      @stop['StopUID']
+    end
+
+    def authority_id
+      @stop['AuthorityID']
+    end
+
+    def stop_name_ch
+      @stop['StopName']['Zh_tw']
+    end
+
+    def stop_name_en
+      @stop['StopName']['En']
+    end
+
+    def stop_latitude
+      @stop['StopPosition']['PositionLat']
+    end
+
+    def stop_longitude
+      @stop['StopPosition']['PositionLon']
+    end
+
+    def stop_address
+      @stop['StopAddress']
+    end
+  end
+end
---
+++ spec/city_stop_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: false
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+require_relative '../lib/motc_api.rb'
+
+describe 'Tests City Stop library' do
+  CITY_NAME = 'Hsinchu'.freeze
+  CORRECT = YAML.safe_load(File.read('spec/fixtures/bs_results.yml'))
+  RESPONSE = YAML.load(File.read('spec/fixtures/bs_response.yml'))
+
+  describe 'City information' do
+    it 'HAPPY: should provide correct city information' do
+      city = PublicTrans::MotcAPI.new(cache: RESPONSE).stop(CITY_NAME)
+      _(city.size).must_equal CORRECT['size']
+      _(city.authority_id).must_equal CORRECT['AuthorityID']
+    end
+
+    it 'SAD: should provide correct message' do
+      proc do
+        PublicTrans::MotcAPI.new(cache: RESPONSE).stop('Tokyo')
+      end.must_raise PublicTrans::MotcAPI::Errors::NotFound
+    end
+  end
+
+  describe 'Bus Stop information' do
+    before do
+      @city = PublicTrans::MotcAPI.new(cache: RESPONSE).stop(CITY_NAME)
+    end
+
+    it 'HAPPY: should provided bus stop list' do
+      stops = @city.bus_stops
+      _(stops.count).must_equal CORRECT['stops'].count
+
+      uid = stops.map(&:uid)
+      correct_uid = CORRECT['stops'].map { |c| c['StopUID'] }
+      _(uid).must_equal correct_uid
+    end
+  end
+end
