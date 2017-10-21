04d07eed0f762510cd7b929104d4895d4d750791
Reggie Gian Escobar <me@reggieescobar.com>
2017-10-21 14:29:30 +0800

    Refactored city stop spec file because of new changes to Motc_Api class

--- spec/city_stop_spec.rb
+++ spec/city_stop_spec.rb
@@ -11,29 +11,28 @@ describe 'Tests City Stop library' do
   RESPONSE = YAML.load(File.read('spec/fixtures/bs_response.yml'))
 
   describe 'City information' do
-    it 'HAPPY: should provide correct city information' do
-      city = PublicTransporation::MotcAPI.new(cache: RESPONSE).stop(CITY_NAME)
+    it 'HAPPY: should provide the correct number of bus stops' do
+      city = PublicTransporation::MotcAPI.new(cache: RESPONSE).city_bus_stops(CITY_NAME)
       _(city.size).must_equal CORRECT['size']
-      _(city.authority_id).must_equal CORRECT['AuthorityID']
     end
 
-    it 'SAD: should provide correct message' do
+    it 'SAD: it should throw a server error message' do
       proc do
-        PublicTransporation::MotcAPI.new(cache: RESPONSE).stop('Tokyo')
+        PublicTransporation::MotcAPI.new(cache: RESPONSE).city_bus_stops('Tokyo')
       end.must_raise PublicTransporation::Errors::ServerError
     end
   end
 
   describe 'Bus Stop information' do
     before do
-      @city = PublicTransporation::MotcAPI.new(cache: RESPONSE).stop(CITY_NAME)
+      @stops = PublicTransporation::MotcAPI.new(cache: RESPONSE).city_bus_stops(CITY_NAME)
     end
 
     it 'HAPPY: should provided bus stop list' do
-      stops = @city.bus_stops
-      _(stops.count).must_equal CORRECT['stops'].count
+      #stops = @city.bus_stops
+      _(@stops.count).must_equal CORRECT['stops'].count
 
-      uid = stops.map(&:uid)
+      uid = @stops.map(&:uid)
       correct_uid = CORRECT['stops'].map { |c| c['StopUID'] }
       _(uid).must_equal correct_uid
     end
