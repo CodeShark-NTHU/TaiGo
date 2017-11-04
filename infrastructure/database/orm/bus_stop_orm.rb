935d0520756f0f6769573f5577e62463092320be
Reggie Gian Escobar <me@reggieescobar.com>
2017-11-05 04:42:26 +0800

    Added bus_stop_orm.rb

--- infrastructure/database/orm/bus_stop_orm.rb
+++ infrastructure/database/orm/bus_stop_orm.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: false
+
+module TaiGo
+    module Database
+        class BusStopOrm < Sequel::Model(:bus_stop)
+
+            many_to_many :routes,
+            join_table: :stops_of_routes,
+            left_key: :stop_uid, right_key: :route_uid
+
+        end
+    end
+end
