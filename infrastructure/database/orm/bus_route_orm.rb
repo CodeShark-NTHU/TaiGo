4f45564745cd907187c46273412c3e6b0fe72792
victorlin12345 <victorlin12345@gmail.com>
2017-11-05 04:44:54 +0800

    Added bus_route_orm.rb file

--- infrastructure/database/orm/bus_route_orm.rb
+++ infrastructure/database/orm/bus_route_orm.rb
@@ -1,1 +1,16 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
+
+module TaiGo
+    module Database
+      # Object-Relational Mapper for bus_route orm 
+      class BusRouteOrm < Sequel::Model(:routes)
+
+        many_to_many :stops,
+                     class: :'TaiGo::Database::BusStopOrm',
+                     join_table: :stops_of_routes,
+                     left_key: :route_uid, right_key: :stop_uid
+                     
+        plugin :timestamps, update_on_create: true
+      end
+    end
+  end
